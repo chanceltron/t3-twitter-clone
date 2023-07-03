495038b66387b703c7995ec68a7404470ee916d6
Chance Ludwick <chanceludwick@gmail.com>
2023-07-03 17:00:33 -0500

    add changes

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,1 +1,193 @@
-export function InfiniteTweetList({ tweets }) {}
+import Link from "next/link";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { ProfileImage } from "./ProfileImage";
+import { useSession } from "next-auth/react";
+import { VscHeartFilled, VscHeart } from "react-icons/vsc";
+import { IconHoverEffect } from "./IconHoverEffect";
+import { api } from "~/utils/api";
+import { LoadingSpinner } from "./LoadingSpinner";
+
+type Tweet = {
+  id: string;
+  content: string;
+  createdAt: Date;
+  likesCount: number;
+  likedByMe: boolean;
+  user: { id: string; image: string | null; name: string | null };
+};
+
+type InfiniteTweetListProps = {
+  isLoading: boolean;
+  isError: boolean;
+  hasMore: boolean | undefined;
+  fetchNewTweets: () => Promise<unknown>;
+  tweets: Tweet[];
+};
+
+export function InfiniteTweetList({
+  tweets,
+  isError,
+  isLoading,
+  fetchNewTweets,
+  hasMore,
+}: InfiniteTweetListProps) {
+  if (isLoading) return <LoadingSpinner />;
+  if (isError) return <h1>Error</h1>;
+  if (tweets == null || tweets.length === 0)
+    return (
+      <h2 className="my-4 text-center text-2xl text-gray-500">No Tweets</h2>
+    );
+
+  return (
+    <ul>
+      <InfiniteScroll
+        dataLength={tweets.length}
+        next={fetchNewTweets}
+        hasMore={hasMore || false}
+        loader={<LoadingSpinner />}
+      >
+        {tweets.map((tweet) => (
+          <TweetCard key={tweet.id} {...tweet} />
+        ))}
+      </InfiniteScroll>
+    </ul>
+  );
+}
+
+const dateTimeFormatted = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "short",
+});
+
+function TweetCard({
+  id,
+  user,
+  content,
+  createdAt,
+  likesCount,
+  likedByMe,
+}: Tweet) {
+  const trpcUtils = api.useContext();
+
+  const toggleLike = api.tweet.toggleLike.useMutation({
+    onSuccess: ({ addedLike }) => {
+      const updateData: Parameters<
+        typeof trpcUtils.tweet.infiniteFeed.setInfiniteData
+      >[1] = (oldData) => {
+        if (oldData == null) return;
+
+        const countModifier = addedLike ? 1 : -1;
+
+        return {
+          ...oldData,
+          pages: oldData.pages.map((page) => {
+            return {
+              ...page,
+              tweets: page.tweets.map((tweet) => {
+                if (tweet.id === id) {
+                  return {
+                    ...tweet,
+                    likesCount: tweet.likesCount + countModifier,
+                    likedByMe: addedLike,
+                  };
+                }
+
+                return tweet;
+              }),
+            };
+          }),
+        };
+      };
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, updateData);
+      trpcUtils.tweet.infiniteFeed.setInfiniteData(
+        { onlyFollowing: true },
+        updateData
+      );
+      trpcUtils.tweet.infiniteProfileFeed.setInfiniteData(
+        { userId: user.id },
+        updateData
+      );
+    },
+  });
+
+  function handleToggleLike() {
+    toggleLike.mutate({ id });
+  }
+
+  return (
+    <li className="flex gap-4 border-b p-4">
+      <Link href={`/profiles/${user.id}`}>
+        <ProfileImage src={user.image} />
+      </Link>
+      <div className="flex flex-grow flex-col">
+        <div className="flex gap-1">
+          <Link
+            href={`/profiles/${user.id}`}
+            className="font-bold outline-none hover:underline focus-visible:underline"
+          >
+            {user.name}
+          </Link>
+          <span className="text-gray-500">-</span>
+          <span className="text-gray-500">
+            {dateTimeFormatted.format(createdAt)}
+          </span>
+        </div>
+        <p className="whitespace-pre-wrap">{content}</p>
+        <HeartButton
+          onClick={handleToggleLike}
+          isLoading={toggleLike.isLoading}
+          likedByMe={likedByMe}
+          likesCount={likesCount}
+        />
+      </div>
+    </li>
+  );
+}
+
+type HeartButtonProps = {
+  onClick: () => void;
+  isLoading: boolean;
+  likedByMe: boolean;
+  likesCount: number;
+};
+
+function HeartButton({
+  likedByMe,
+  likesCount,
+  isLoading,
+  onClick,
+}: HeartButtonProps) {
+  const session = useSession();
+  const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
+  if (session.status !== "authenticated") {
+    return (
+      <div className="my-1 flex items-center gap-3 self-start text-gray-500">
+        <HeartIcon />
+        <span>{likesCount}</span>
+      </div>
+    );
+  }
+
+  return (
+    <button
+      disabled={isLoading}
+      onClick={onClick}
+      className={`group -ml-2 flex items-center gap-1 self-start transition-colors duration-200 ${
+        likedByMe
+          ? "text-red-500"
+          : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+      }`}
+    >
+      <IconHoverEffect red>
+        <HeartIcon
+          className={`transition-colors duration-200 ${
+            likedByMe
+              ? "fill-red-500"
+              : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
+          }`}
+        />
+      </IconHoverEffect>
+      <span>{likesCount}</span>
+    </button>
+  );
+}
